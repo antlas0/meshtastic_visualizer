5447829369fab4f3068b12dc6e7be99d0a617385
antlas0 <150958268+antlas0@users.noreply.github.com>
2024-08-25 10:27:22 +0200

    bump to 1.2.0

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
 from setuptools import setup, find_packages
 
 # Function to read the requirements.txt file
+
+
 def parse_requirements(filename):
     with open(filename, 'r') as file:
         lines = file.read().splitlines()
         # Filter out comments and empty lines
-        requirements = [line for line in lines if line and not line.startswith('#')]
+        requirements = [
+            line for line in lines if line and not line.startswith('#')]
     return requirements
+
 
 # Parse requirements.txt
 requirements = parse_requirements('requirements.txt')
 
 setup(
     name="meshtastic_visualizer",
-    version="1.1.0",
+    version="1.2.0",
     packages=find_packages(),
     scripts=['bin/meshtastic_visualizer'],
     install_requires=requirements,
