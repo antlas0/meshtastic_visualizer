c3b6352fdf6b115319516ff41b53e6082040b8be
antlas0 <150958268+antlas0@users.noreply.github.com>
2025-03-23 21:37:28 +0100

    conforming to generic function signature

--- meshtastic_visualizer/meshtastic_manager.py
+++ meshtastic_visualizer/meshtastic_manager.py
@@ -206,8 +206,7 @@ class MeshtasticManager(QObject, threading.Thread):
 
     @run_in_thread
     def on_receive(self, packet: dict,
-                   interface: Optional[meshtastic.serial_interface.SerialInterface] = None,
-                   node_list: Optional[list]=None):
+                   interface):
         if "decoded" not in packet:
             return
         if "portnum" not in packet["decoded"]:
