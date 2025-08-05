2962f2c271e5753dd584f73a8aa1ad13111de814
antlas0 <150958268+antlas0@users.noreply.github.com>
2025-08-05 21:18:12 +0200

    add custom widget to see packets and telemetry

---
+++ meshtastic_visualizer/packet_viewer_widget.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from PyQt6.QtWidgets import QWidget, QPushButton, QHBoxLayout
+
+class PacketViewerWidget(QWidget):
+    def __init__(self, parent, callback_packet_viewer, callback_telemetry_viewer, node_id:str=""):
+        super(PacketViewerWidget,self).__init__(parent)
+        self._node_id = node_id
+        self.parent = parent
+        layout = QHBoxLayout()
+        layout.setContentsMargins(0,0,0,0)
+        layout.setSpacing(0)
+
+        btn = QPushButton("See telemetry")
+        btn.setStyleSheet("QPushButton{font-size: 9pt;}")
+        btn.setEnabled(True)
+        btn.clicked.connect(lambda: callback_telemetry_viewer(self._node_id))
+        layout.addWidget(btn)
+
+        btn = QPushButton("See packets")
+        btn.setStyleSheet("QPushButton{font-size: 9pt;}")
+        btn.setEnabled(True)
+        btn.clicked.connect(lambda: callback_packet_viewer(self._node_id))
+        layout.addWidget(btn)
+
+        self.setLayout(layout)
+    
