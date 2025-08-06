74f186109c96d7e41c987ae3b30e231e3bee2ec1
antlas0 <150958268+antlas0@users.noreply.github.com>
2025-08-06 21:22:06 +0200

    if no telemetry received, no btn associated

--- meshtastic_visualizer/packet_viewer_widget.py
+++ meshtastic_visualizer/packet_viewer_widget.py
@@ -3,7 +3,7 @@
 from PyQt6.QtWidgets import QWidget, QPushButton, QHBoxLayout
 
 class PacketViewerWidget(QWidget):
-    def __init__(self, parent, callback_packet_viewer, callback_telemetry_viewer, node_id:str=""):
+    def __init__(self, parent, callback_packet_viewer, callback_telemetry_viewer, has_telemetry:bool, node_id:str=""):
         super(PacketViewerWidget,self).__init__(parent)
         self._node_id = node_id
         self.parent = parent
@@ -11,11 +11,12 @@ class PacketViewerWidget(QWidget):
         layout.setContentsMargins(0,0,0,0)
         layout.setSpacing(0)
 
-        btn = QPushButton("See telemetry")
-        btn.setStyleSheet("QPushButton{font-size: 9pt;}")
-        btn.setEnabled(True)
-        btn.clicked.connect(lambda: callback_telemetry_viewer(self._node_id))
-        layout.addWidget(btn)
+        if has_telemetry:
+            btn = QPushButton("See telemetry")
+            btn.setStyleSheet("QPushButton{font-size: 9pt;}")
+            btn.setEnabled(True)
+            btn.clicked.connect(lambda: callback_telemetry_viewer(self._node_id))
+            layout.addWidget(btn)
 
         btn = QPushButton("See packets")
         btn.setStyleSheet("QPushButton{font-size: 9pt;}")
